e3965cb0524ca5f923a800c1574305107ece4a24
fitnicely <nasikelabu22@gmail.com>
2023-04-03 10:06:35 +0800

    UPDATE Application Test Code

--- FITNICELY_application-code_UPDATED/app/src/main/java/com/week8/ZumbaPage.java
+++ FITNICELY_application-code_UPDATED/app/src/main/java/com/week8/ZumbaPage.java
@@ -25,36 +25,4 @@ public class ZumbaPage extends AppCompatActivity {
         zumba_expert.playVidExpert(findViewById(R.id.video_view3),"android.resource://" + getPackageName() + "/" + R.raw.zumba3, mediaController3 );
 
     }
-
-
-   /* @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_zumba_easy);
-
-        VideoView zumba_easy = findViewById(R.id.video_view);
-        VideoView zumba_intermediate = findViewById(R.id.video_view2);
-        VideoView zumba_expert = findViewById(R.id.video_view3);
-
-        zumba_easy.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.zumba));
-        zumba_easy.start();
-
-        zumba_intermediate.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.zumba2));
-
-        zumba_expert.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.zumba3));
-
-        MediaController mediaController = new MediaController(this);
-        zumba_easy.setMediaController(mediaController);
-        mediaController.setAnchorView(zumba_easy);
-
-        MediaController mediaController2 = new MediaController(this);
-        zumba_intermediate.setMediaController(mediaController2);
-        mediaController2.setAnchorView(zumba_intermediate);
-
-        MediaController mediaController3 = new MediaController(this);
-        zumba_expert.setMediaController(mediaController3);
-        mediaController3.setAnchorView(zumba_expert);
-
-
-    }*/
 }
--- FITNICELY_application-code_UPDATED/app/src/main/java/com/week8/pilatesPage.java
+++ FITNICELY_application-code_UPDATED/app/src/main/java/com/week8/pilatesPage.java
@@ -27,35 +27,3 @@ public class pilatesPage extends AppCompatActivity {
 
     }
 }
-
-
-   /* @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pilates_easy);
-
-        VideoView pilates_easy = findViewById(R.id.video_view);
-        VideoView pilates_intermediate = findViewById(R.id.video_view2);
-        VideoView pilates_expert = findViewById(R.id.video_view3);
-
-        pilates_easy.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video));
-        pilates_easy.start();
-
-        pilates_intermediate.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video2));
-        // pilates_intermediate.start();
-
-        pilates_expert.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.video3));
-
-        MediaController mediaController = new MediaController(this);
-        pilates_easy.setMediaController(mediaController);
-        mediaController.setAnchorView(pilates_easy);
-
-        MediaController mediaController2 = new MediaController(this);
-        pilates_intermediate.setMediaController(mediaController2);
-        mediaController2.setAnchorView(pilates_intermediate);
-
-        MediaController mediaController3 = new MediaController(this);
-        pilates_expert.setMediaController(mediaController3);
-        mediaController3.setAnchorView(pilates_expert);
-    }
-}*/
